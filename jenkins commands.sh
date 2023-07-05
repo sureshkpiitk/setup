5039f9bef403ae5695f492334065c45bf12b39df
Suresh Prajapat <suresh.prajapat@synapsefi.com>
2023-07-05 13:29:08 +0530

    updated commends

--- jenkins commands.sh
+++ jenkins commands.sh
@@ -11,20 +11,31 @@ docker run \
 
 
 
-docker  run --privileged -d -p 8082:8080 \
-    -v jenkinsvol1:/var/jenkins_home \
-    --name jenkins-local \
-    --user root \
-   jenkins-custom
+docker run \
+  --name jenkins-blueocean \
+  --restart=on-failure \
+  --detach \
+  --network jenkins \
+  --env DOCKER_HOST=tcp://docker:2376 \
+  --env DOCKER_CERT_PATH=/certs/client \
+  --env DOCKER_TLS_VERIFY=1 \
+  --publish 8082:8080 \
+  --publish 50000:50000 \
+  --volume jenkins-data:/var/jenkins_home \
+  --volume jenkins-docker-certs:/certs/client:ro \
+  jenkins-custom
 
   
-  docker run \
+ docker run \
   --name jenkins-docker \
   --rm \
   --detach \
   --privileged \
+  --network jenkins \
+  --network-alias docker \
+  --env DOCKER_TLS_CERTDIR=/certs \
   --volume jenkins-docker-certs:/certs/client \
   --volume jenkins-data:/var/jenkins_home \
-  --publish 8002:2376 \
+  --publish 2376:2376 \
   docker:dind \
   --storage-driver overlay2
